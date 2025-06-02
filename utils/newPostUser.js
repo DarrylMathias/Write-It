7305fb58d01ad426e4ef7ce1747d85cd723d55cb
DarrylMathias <mathiasndarryl7@gmail.com>
2025-06-03 01:28:14 +0530

    Edit profile fixed

---
+++ utils/newPostUser.js
@@ -0,0 +1,39 @@
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+dotenv.config()
+
+const sendUserMail = function (email, name, title, content) {
+    try {
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.APP_PASSWORD,
+            },
+        });
+
+        const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: `New User, ${name} | ${email}, (${age})`,
+            html: `
+                <h2>New User Update</h2>
+                <strong>Name:</strong> ${name}<br>
+                <strong>Email:</strong> ${email}<br>
+                <strong>Age:</strong> ${age}<br><br>
+            `
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('Email error:', error);
+            } else {
+                console.log('Email sent:', info.response);
+            }
+        });
+    } catch (err) {
+        console.error("Failed to send email:", err);
+    }
+}
+
+module.exports = sendUserMail
