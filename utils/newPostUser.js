7c4a3a2bf28b5a06a48f3d2ff162576847167a7a
Darryl Mathias <darrylnevmat@gmail.com>
2025-11-27 01:52:32 +0530

    increased sleep period between emails

--- utils/newPostUser.js
+++ utils/newPostUser.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 const userModel = require('../models/user')
 
-const {Resend} = require('resend')
+const { Resend } = require('resend')
 dotenv.config()
 
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -11,6 +11,7 @@ const newPostUser = async function (author, title, content) {
     const allUsers = await userModel.find()
     allUsers.forEach(async (user) => {
         try {
+            setTimeout(() => { }, 1000);
             const { data, error } = await resend.emails.send({
                 from: 'Darryl <help@darrylmathias.tech>',
                 to: [user.email],
@@ -19,7 +20,7 @@ const newPostUser = async function (author, title, content) {
                     <h2>${title}</h2>
                     <p>${content}</p><br>
                     <p>Post by ${author}</p>
-                    <p>Mail from team Write It</p>
+                    <p>Mail from team <a href='https://write-it-dz2w.onrender.com/'>Write It</a></p>
                 `
             });
 
